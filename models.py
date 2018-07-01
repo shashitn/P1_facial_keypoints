e26b15f3ca2bf0bb0c4716f993b39330a60dc828
Bhushan, Shashi T <shashi.t.bhushan@accenture.com>
2018-07-01 10:02:53 +0000

    final commit

---
+++ models.py
@@ -0,0 +1,79 @@
+## TODO: define the convolutional neural network architecture
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+# can use the below import should you choose to initialize the weights of your Net
+import torch.nn.init as I
+
+'''LeNet in PyTorch.'''
+class LeNet(nn.Module):
+    def __init__(self):
+        super(LeNet, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1   = nn.Linear(16*5*5, 120)
+        self.fc2   = nn.Linear(120, 84)
+        self.fc3   = nn.Linear(84, 10)
+
+    def forward(self, x):
+        out = F.relu(self.conv1(x))
+        out = F.max_pool2d(out, 2)
+        out = F.relu(self.conv2(out))
+        out = F.max_pool2d(out, 2)
+        out = out.view(out.size(0), -1)
+        out = F.relu(self.fc1(out))
+        out = F.relu(self.fc2(out))
+        out = self.fc3(out)
+        return out
+
+
+class Net(nn.Module):
+
+    def __init__(self):
+        super(Net, self).__init__()
+        
+        ## TODO: Define all the layers of this CNN, the only requirements are:
+        ## 1. This network takes in a square (same width and height), grayscale image as input
+        ## 2. It ends with a linear layer that represents the keypoints
+        ## it's suggested that you make this last layer output 136 values, 2 for each of the 68 keypoint (x, y) pairs
+        
+        # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
+        # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
+        self.conv1 = nn.Conv2d(1, 32, 5)
+        self.conv2 = nn.Conv2d(32, 64, 5)
+        self.conv3 = nn.Conv2d(64, 128, 5)
+        self.conv4 = nn.Conv2d(128, 256, 5)
+        self.max_pool = nn.MaxPool2d(2,2)
+        self.dense1 = nn.Linear(25600, 1024)
+        self.dense2 = nn.Linear(1024, 136)
+        self.drop = nn.Dropout(p=0.5)
+        
+        ## Note that among the layers to add, consider including:
+        # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
+        
+
+        
+    def forward(self, x):
+        ## TODO: Define the feedforward behavior of this model
+        ## x is the input image and, as an example, here you may choose to include a pool/conv step:
+        x = self.max_pool(F.relu(self.conv1(x)))
+#         print(x.shape)
+        x = self.max_pool(F.relu(self.conv2(x)))
+#         print(x.shape)
+        x = self.max_pool(F.relu(self.conv3(x)))
+#         print(x.shape)
+        x = self.max_pool(F.relu(self.conv4(x)))
+#         print(x.shape)
+        
+        x = x.view(x.size(0), -1)
+#         print(x.shape)
+        x = F.relu(self.dense1(x))
+        #x = self.fc1_bn(x)
+        x = self.drop(x)
+
+        x = self.dense2(x)
+        
+        
+        # a modified x, having gone through all the layers of your model, should be returned
+        return x
